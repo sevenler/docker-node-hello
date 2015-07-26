96e3bbc2336108b1f475786d2e342ea7617ebeb7
sevenler <johnnyxyz@163.com>
2015-07-26 10:13:19 +0800

    edit index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  res.send('This is Johnny\n');
 });
 
 app.listen(PORT)
