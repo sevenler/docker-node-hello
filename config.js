623438b0202bf37f62bba2db7ed60f4d583efa5e
johnnyxyz <johnnyxyz@163.com>
2015-08-01 15:47:19 +0800

    edit

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var config = {
 
 var mongo_address = process.env['MONGO_PORT_27017_TCP_ADDR'];
 var mongo_port = process.env['MONGO_PORT_27017_TCP_PORT'];
-var mongo_name = process.env['MONGO_NAME'];
-var db_connect_string = mongo_address + ":" mongo_port + mongo_name;
-
-console.log("=========", db_connect_string);
+var mongo_name = "/test";
+var db_connect_string = mongo_address + ":" +  mongo_port + mongo_name;
 config.db = db_connect_string;
+
+module.exports = config;
---
+++ run.sh
@@ -0,0 +1,4 @@
+docker run -p 80:80 \
+ --link mongo:mongo -t -i \
+johnny/docker-hello \
+#/bin/bash
