dab080b1f288a4b4a0bb1f4130ab7a72a7f51839
sevenler <johnnyxyz@163.com>
2015-08-01 14:18:32 +0800

    edit config.js

--- config.js
+++ config.js
@@ -3,4 +3,10 @@ var config = {
 	db: 'uozDROq0Vcg5QkT6:pbAMjWfdc8DgSomT6@10.10.72.139:27017/Ph17LyIrTEpl9xaz'
 };
 
-config.db = process.env['MONGO_PORT'];
+var mongo_address = process.env['MONGO_PORT_27017_TCP_ADDR'];
+var mongo_port = process.env['MONGO_PORT_27017_TCP_PORT'];
+var mongo_name = process.env['MONGO_NAME'];
+var db_connect_string = mongo_address + ":" mongo_port + mongo_name;
+
+console.log("=========", db_connect_string);
+config.db = db_connect_string;
